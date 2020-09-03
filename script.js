2ecf01fcb012f6ba00dafb97ecd9100f2f20f4fe
Pradeepj2 <70266771+Pradeepj2@users.noreply.github.com>
2020-09-03 15:37:38 +0530

    final soution module 4

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var namePr = ["Akash","kaushal","jatin","shudhanshu"];
+
+for(var i=0;i<namePr.length;i++){
+    if(namePr[i][0]=="j")
+    {
+        console.log("Goodbye "+namePr[i]);
+    }
+    else{
+        console.log("Hello "+namePr[i]);
+    }
+}
